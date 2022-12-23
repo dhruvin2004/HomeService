9c1e8bbc53783c158692657a42042eec2d79f902
dhruvin pandav <dhruvinpandav999@gmail.com>
2022-12-23 23:08:45 +0530

    new commit

--- lib/scrrens/order.dart
+++ lib/scrrens/order.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:home_service/scrrens/variable.dart';
 
 class Order extends StatefulWidget {
@@ -23,58 +24,106 @@ class _OrderState extends State<Order> {
             children: [
               ...Global.myOrder
                   .map(
-                    (e) => Container(
-                      height: 150,
-                      width: w,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          color: Colors.grey.shade200,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Row(
+                    (e) => Slidable(
+                      key: const ValueKey(0),
+                      endActionPane: ActionPane(
+                        motion: const ScrollMotion(),
                         children: [
-                          CircleAvatar(
-                            radius: 50,
-                            backgroundColor: Colors.grey,
-                            backgroundImage: AssetImage(e['image']),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Spacer(),
-                              Row(
-                                children: [
-                                  Text(
-                                    e['name'],
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                e['service'],
-                                style: TextStyle(
-                                    color: Colors.grey.shade400, fontSize: 16),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                "₹ ${e['charge'].toString()}",
-                                style: TextStyle(
-                                    color: Colors.red,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Spacer(),
-                            ],
+                          SlidableAction(
+                            flex: 2,
+                            onPressed: (val) {
+                              setState(() {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return CupertinoAlertDialog(
+                                        title: const Text(
+                                            "Are you sure for cancel you order?"),
+                                        insetAnimationCurve: Curves.linear,
+                                        actions: [
+                                          CupertinoButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                Global.myOrder.remove(e);
+                                                Navigator.pop(context);
+                                              });
+                                            },
+                                            child: const Text("Yes"),
+                                          ),
+                                          CupertinoButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                Navigator.pop(context);
+                                              });
+                                            },
+                                            child: const Text("No"),
+                                          ),
+                                        ],
+                                      );
+                                    });
+                              });
+                            },
+                            backgroundColor: Colors.red,
+                            foregroundColor: Colors.white,
+                            icon: Icons.delete,
+                            label: 'Delete',
                           ),
                         ],
+                      ),
+                      child: Container(
+                        height: 150,
+                        width: w,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 50,
+                              backgroundColor: Colors.grey,
+                              backgroundImage: AssetImage(e['image']),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Spacer(),
+                                Row(
+                                  children: [
+                                    Text(
+                                      e['name'],
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  e['service'],
+                                  style: TextStyle(
+                                      color: Colors.grey.shade400,
+                                      fontSize: 16),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  "₹ ${e['charge'].toString()}",
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Spacer(),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   )
